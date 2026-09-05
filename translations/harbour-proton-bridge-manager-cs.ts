5edeafa77def1f65874ad36c7a6c46f1fdea5f02
Dominik Chrastecky <dominik@chrastecky.cz>
2026-09-05 21:35:06 +0200

    Add czech translation

--- translations/harbour-proton-bridge-manager-cs.ts
+++ translations/harbour-proton-bridge-manager-cs.ts
@@ -1,99 +1,99 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="cs">
 <context>
     <name></name>
     <message id="check.error.stopping">
         <location filename="../qml/pages/CheckStatusPage.qml" line="34"/>
         <source>Stopping the bridge failed, please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zastavení bridge selhalo, zkuste to prosím později.</translation>
     </message>
     <message id="check.pull_down.stop_bridge">
         <location filename="../qml/pages/CheckStatusPage.qml" line="65"/>
         <source>Stop the bridge</source>
-        <translation type="unfinished"></translation>
+        <translation>Zastavit bridge</translation>
     </message>
     <message id="check.title">
         <location filename="../qml/pages/CheckStatusPage.qml" line="81"/>
         <source>Checking bridge status</source>
-        <translation type="unfinished"></translation>
+        <translation>Zjišťování stavu bridge</translation>
     </message>
     <message id="check.loading_status">
         <location filename="../qml/pages/CheckStatusPage.qml" line="88"/>
         <source>The current bridge status is being loaded.</source>
-        <translation type="unfinished"></translation>
+        <translation>Momentálně je získáván stav bridge.</translation>
     </message>
     <message id="check.bridge_unknown_status">
         <location filename="../qml/pages/CheckStatusPage.qml" line="97"/>
         <source>The bridge did not report any known status, cannot continue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bridge nenahlásil žádný známý status, nelze pokračovat.</translation>
     </message>
     <message id="check.bridge_is_started">
         <location filename="../qml/pages/CheckStatusPage.qml" line="116"/>
         <source>The bridge is currently started, please stop the bridge using the pull down menu before continuing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bridge momentálně běží, prosím zastavte jej potáhnutím dolů, než budete pokračovat.</translation>
     </message>
     <message id="main.loading">
         <location filename="../qml/pages/MainPage.qml" line="16"/>
         <source>Loading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Načítání...</translation>
     </message>
     <message id="main.error.starting_bridge">
         <location filename="../qml/pages/MainPage.qml" line="34"/>
         <source>Starting the bridge failed, please try again.</source>
         <oldsource>Starting the bridg failed, please try again.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Zapínání bridge selhalo, zkuste to prosím znovu.</translation>
     </message>
     <message id="main.error.account_fetch">
         <location filename="../qml/pages/MainPage.qml" line="41"/>
         <source>Failed to fetch a list of accounts. Please close and reopen the app.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nezdařilo se získat seznam účtů. Prosím zavřete a znovu otevřete aplikaci.</translation>
     </message>
     <message id="main.error.totp_required">
         <location filename="../qml/pages/MainPage.qml" line="52"/>
         <source>You either forgot to include totp or it was invalid.</source>
-        <translation type="unfinished"></translation>
+        <translation>Buď jste zapomněli vyplnit 2fázové ověření, nebo bylo neplatné.</translation>
     </message>
     <message id="main.error.login_failed">
         <location filename="../qml/pages/MainPage.qml" line="60"/>
         <source>Something went wrong when logging in. Please try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Něco se při přihlašování pokazilo. Zkuste to prosím znovu.</translation>
     </message>
     <message id="main.error.remove_failed">
         <location filename="../qml/pages/MainPage.qml" line="71"/>
         <source>Something went wrong when removing the account. Please try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Něco se při odstraňování účtu pokazilo. Zkuste to prosím znovu.</translation>
     </message>
     <message id="main.start_bridge">
         <location filename="../qml/pages/MainPage.qml" line="93"/>
         <source>Start the bridge</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapnout bridge</translation>
     </message>
     <message id="main.add_accounts">
         <location filename="../qml/pages/MainPage.qml" line="103"/>
         <source>Add an account</source>
-        <translation type="unfinished"></translation>
+        <translation>Přidat účet</translation>
     </message>
     <message id="main.title">
         <location filename="../qml/cover/CoverPage.qml" line="44"/>
         <location filename="../qml/pages/MainPage.qml" line="116"/>
         <source>ProtonBridge Control</source>
-        <translation type="unfinished"></translation>
+        <translation>ProtonBridge Control</translation>
     </message>
     <message id="main.no_accounts">
         <location filename="../qml/pages/MainPage.qml" line="125"/>
         <source>No accounts are currently configured, use the pull down menu to add one.</source>
-        <translation type="unfinished"></translation>
+        <translation>Momentálně nejsou nastaveny žádné účty, potáhněte dolů pro přidání účtu.</translation>
     </message>
     <message id="main.account.credentials">
         <location filename="../qml/pages/MainPage.qml" line="154"/>
         <source>Account credentials</source>
-        <translation type="unfinished"></translation>
+        <translation>Údaje o účtu</translation>
     </message>
     <message id="main.account.username">
         <location filename="../qml/pages/MainPage.qml" line="167"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Uživatelské jméno</translation>
     </message>
     <message id="main.account.copied">
         <location filename="../qml/pages/MainPage.qml" line="190"/>
@@ -101,68 +101,68 @@
         <location filename="../qml/pages/MainPage.qml" line="278"/>
         <location filename="../qml/pages/MainPage.qml" line="313"/>
         <source>Copied to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Zkopírováno do schránky</translation>
     </message>
     <message id="main.account.password">
         <location filename="../qml/pages/MainPage.qml" line="203"/>
         <source>Bridge password</source>
-        <translation type="unfinished"></translation>
+        <translation>Heslo bridge</translation>
     </message>
     <message id="main.account.connection_ports">
         <location filename="../qml/pages/MainPage.qml" line="238"/>
         <source>Connection ports</source>
-        <translation type="unfinished"></translation>
+        <translation>Porty</translation>
     </message>
     <message id="main.account.imap_port">
         <location filename="../qml/pages/MainPage.qml" line="255"/>
         <source>IMAP port</source>
-        <translation type="unfinished"></translation>
+        <translation>IMAP port</translation>
     </message>
     <message id="main.account.smtp_port">
         <location filename="../qml/pages/MainPage.qml" line="290"/>
         <source>SMTP port</source>
-        <translation type="unfinished"></translation>
+        <translation>SMTP port</translation>
     </message>
     <message id="main.account.starttls_info">
         <location filename="../qml/pages/MainPage.qml" line="328"/>
         <source>Use STARTTLS for both IMAP and SMTP connections.</source>
-        <translation type="unfinished"></translation>
+        <translation>Použijte STARTTLS pro IMAP i SMTP.</translation>
     </message>
     <message id="main.account.remove">
         <location filename="../qml/pages/MainPage.qml" line="335"/>
         <location filename="../qml/pages/MainPage.qml" line="341"/>
         <source>Remove account</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstranit účet</translation>
     </message>
     <message id="add.accept_text">
         <location filename="../qml/pages/AddAccountPage.qml" line="26"/>
         <source>Create account</source>
-        <translation type="unfinished"></translation>
+        <translation>Vytvořit účet</translation>
     </message>
     <message id="add.cancel_text">
         <location filename="../qml/pages/AddAccountPage.qml" line="28"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Zrušit</translation>
     </message>
     <message id="add.username.label">
         <location filename="../qml/pages/AddAccountPage.qml" line="33"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Uživatelské jméno</translation>
     </message>
     <message id="add.password.label">
         <location filename="../qml/pages/AddAccountPage.qml" line="40"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Heslo</translation>
     </message>
     <message id="add.totp.label">
         <location filename="../qml/pages/AddAccountPage.qml" line="47"/>
         <source>TOTP code</source>
-        <translation type="unfinished"></translation>
+        <translation>Kód 2FA</translation>
     </message>
     <message id="add.totp.description">
         <location filename="../qml/pages/AddAccountPage.qml" line="49"/>
         <source>Only provide the code if you have an authenticator configured.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zadejte kód jen v případě, že máte nastaveno dvoufázové ověření.</translation>
     </message>
 </context>
 </TS>
